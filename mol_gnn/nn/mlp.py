5664f765eaac82bccc95160277a32d2988df704f
david graff <davidegraff62@gmail.com>
2024-12-02 13:19:36 -0500

    delete some old code and fix uv config

--- mol_gnn/nn/mlp.py
+++ mol_gnn/nn/mlp.py
@@ -6,21 +6,21 @@ def MLP(
     hidden_dim: int = 300,
     n_layers: int = 1,
     dropout: float = 0.0,
-    activation: type[nn.Module] = nn.ReLU(),
+    activation: type[nn.Module] = nn.ReLU,
 ) -> nn.Sequential:
     r"""An :class:`MLP` is an FFN that implements the following function:
 
     .. math::
         \mathbf h_0 &= \mathbf x\,\mathbf W^{(0)} + \mathbf b^{(0)} \\
         \mathbf h_l &= \mathtt{dropout} \left(
-            \sigma \left(\,\mathbf h_{l-1}\,\mathbf W^{{l)} \right)
+            \tau \left(\,\mathbf h_{l-1}\,\mathbf W^{{l)} \right)
         \right) \\
         \mathbf h_L &= \mathbf h_{L-1} \mathbf W^{{l)} + \mathbf b^{{l)},
 
     where :math:`\mathbf x` is the input tensor, :math:`\mathbf W^{{l)}` is the learned weight
     matrix for the :math:`l`-th layer, :math:`\mathbf b^{{l)}` is the bias vector for the
     :math:`l`-th layer, :math:`\mathbf h^{{l)}` is the hidden representation at layer :math:`l`,
-    :math:`\sigma` is the activation function, and :math:`L` is the number of layers.
+    :math:`\tau` is a nonlinearity (e.g., ReLU), and :math:`L` is the number of layers.
     """
 
     dropout = nn.Dropout(dropout)
