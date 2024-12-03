ead2ea7335c70acab6a5d444eb2b5f6c3d3b53c9
david graff <davidegraff62@gmail.com>
2024-12-03 17:15:45 -0500

    add generic model

--- mol_gnn/types.py
+++ mol_gnn/types.py
@@ -1,4 +1,11 @@
 # ruff: noqa: F401
 from rdkit.Chem import Mol, Atom, Bond
+from torch import nn
 
-Rxn = tuple[Mol, Mol]
+type Rxn = tuple[Mol, Mol]
+
+type TensorDictKey = tuple[str, ...] | str
+type ModelConfig = dict[
+    str, tuple[nn.Module, list[TensorDictKey] | dict[TensorDictKey, str], list[TensorDictKey]]
+]
+type LossConfig = dict[str, tuple[float, nn.Module, list[TensorDictKey] | dict[TensorDictKey, str]]]
