a5cecf27e29f8fd3e806f8c230746861c2ccc311
david graff <davidegraff62@gmail.com>
2024-12-05 12:54:19 -0500

    refactor lr scheduling

--- mol_gnn/types.py
+++ mol_gnn/types.py
@@ -1,7 +1,9 @@
 # ruff: noqa: F401
-from typing import Callable, TypedDict
+from typing import Callable, Literal, Required, TypedDict
+
 from rdkit.Chem import Mol, Atom, Bond
-from torch import Tensor, nn
+from torch import Tensor
+from torch.optim.lr_scheduler import LRScheduler
 
 type Rxn = tuple[Mol, Mol]
 type TensorDictKey = tuple[str, ...] | str
@@ -17,3 +19,12 @@ class LossModuleConfig(TypedDict):
     weight: float
     module: Callable[..., Tensor]
     in_keys: list[TensorDictKey] | dict[TensorDictKey, str]
+
+
+class LRSchedConfig(TypedDict, total=False):
+    scheduler: Required[LRScheduler]
+    interval: Literal["step", "epoch"]
+    frequency: int
+    monitor: str
+    strict: bool
+    name: str | None
