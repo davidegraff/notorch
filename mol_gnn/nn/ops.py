2c8bad04aa495f17dcc65ffe8c19c677ebb691f2
david graff <davidegraff62@gmail.com>
2024-12-20 11:00:18 -0500

    add `MatMul`

--- mol_gnn/nn/ops.py
+++ mol_gnn/nn/ops.py
@@ -23,11 +23,41 @@ class Prod(_OpBase):
 
 
 class Cat(_OpBase):
+    """Concatenate the input tensors along :attr:`dim`
+
+    Parameters
+    ----------
+    dim : int, default=-1
+        the dimension along which to concatenate
+    """
     def __init__(self, dim: int = -1):
         super().__init__(dim)
 
     def forward(self, *tensors: Tensor) -> Tensor:
         return torch.cat(tensors, dim=self.dim)
+
+
+class MatMul(nn.Module):
+    """
+
+    Parameters
+    ----------
+    transpose : bool, default False
+        whether to transpose the last two dimensions of :attr:`B`
+    """
+    def __init__(self, transpose: bool = False) -> None:
+        super().__init__()
+
+        self.transpose = transpose
+
+    def forward(self, A: Tensor, B: Tensor) -> Tensor:
+        if self.transpose:
+            B = B.mT
+
+        return A @ B
+
+    def extra_repr(self) -> str:
+        return f"transpose={self.transpose}"
 
 
 class Einsum(nn.Module):
