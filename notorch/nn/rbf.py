9a7756af979ba333fede16cf18db5386a93f61fc
david graff <davidegraff62@gmail.com>
2025-01-08 12:54:03 -0500

    add rbf layer

---
+++ notorch/nn/rbf.py
@@ -0,0 +1,25 @@
+from jaxtyping import Float
+import torch
+from torch import Tensor
+import torch.nn as nn
+
+
+class RBFEmbedding(nn.Module):
+    means: Float[Tensor, "1 n_bases"]
+
+    def __init__(self, d_min: float, d_max: float, num_bases: int):
+        super().__init__()
+
+        means = torch.linspace(d_min, d_max, num_bases)
+        width = (d_min - d_max) / num_bases
+
+        self.register_buffer("means", means.unsqueeze(0))
+        self.width = width
+
+    def forward(self, D: Float[Tensor, "b"]) -> Float[Tensor, "b n_bases"]:
+        diffs = D.unsqueeze(-1) - self.means
+
+        return torch.exp(-(diffs / self.width) ** 2)
+
+    def extra_repr(self) -> str:
+        return f"d_min={self.means[0]}, d_max={self.means[-1]}, num_bases={len(self.means)}"
