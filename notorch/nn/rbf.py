fix calculation

notorch/nn/rbf.py
@@ -14,12 +14,12 @@ class RBFEmbedding(nn.Module):
         width = (d_min - d_max) / num_bases
 
         self.register_buffer("means", means.unsqueeze(0))
-        self.width = width
+        self.width = 0.5 * 1 / width**2
 
     def forward(self, D: Float[Tensor, "b"]) -> Float[Tensor, "b n_bases"]:
         diffs = D.unsqueeze(-1) - self.means
 
-        return torch.exp(-(diffs / self.width) ** 2)
+        return torch.exp(-self.factor *  diffs**2)
 
     def extra_repr(self) -> str:
         return f"d_min={self.means[0]}, d_max={self.means[-1]}, num_bases={len(self.means)}"